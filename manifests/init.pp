ce5ecd97b6640b36fe94d73cdd8a272dc4635e13
Justin Hoppensteadt <hop@nationbuilder.com>
2015-05-18 15:57:24 -0700

    Trying other style for class

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,19 +36,9 @@
 # Copyright 2015 3dna
 #
 class mms {
-  include ::mms::install
-  include ::mms::config
-  include ::mms::service
-
-  Class["::mms::install"] -> Class["::mms::config"] ~> Class["::mms::service"]
-
-  anchor {
-    'mms::begin':
-      before => [Class['::mms::install'],Class['::mms::config']],
-      notify => Class['::mms::service'];
-    'mms::end':
-      require => Class['::mms::service'];
-  }
+  include mms::install
+  include mms::config
+  include mms::service
 
 }
 # vim: ft=puppet
