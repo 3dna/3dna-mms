903bf7c531418e4bb055dce7032731b7bb933af2
Justin Hoppensteadt <hop@nationbuilder.com>
2015-05-18 10:55:35 -0700

    Formatting and name fix

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,10 +2,10 @@
 class mms::service inherits mms {
 
   service {'mongodb-mms-automation-agent':
-    ensure => running,
-    enable => true,
-    provider => 'upstart',
-    require => [
+    ensure    => running,
+    enable    => true,
+    provider  => 'upstart',
+    require   => [
       Package['mongodb-mms-automation-agent-manager'],
       File['/etc/mongodb-mms/automation-agent.config']
     ],
