c23a1b4d9fdce197f89df80269843dc006aad5c0
Justin Hoppensteadt <git@justinhoppensteadt.com>
2015-05-19 23:45:05 +0000

    fixed some syntax

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,40 +3,40 @@ class mms::config inherits mms {
 
   file {
   '/etc/mongodb-mms/automation-agent.config':
-    content => template($module_name/automation-agent.config.erb),
-    owner  => 'mongodb',
-    group  => 'mongodb',
-    mode   => '0644',
+    content => template($module_name << '/automation-agent.config.erb'),
+    owner   => 'mongodb',
+    group   => 'mongodb',
+    mode    => '0644',
     require => Package['mongodb-mms-automation-agent-manager'];
   '/opt/mongodb-mms-automation':
-    ensure => 'directory',
-    owner => 'mongodb',
-    group  => 'mongodb',
-    mode   => '0775',
+    ensure  => 'directory',
+    owner   => 'mongodb',
+    group   => 'mongodb',
+    mode    => '0775',
     require => Package['mongodb-mms-automation-agent-manager'];
   '/opt/mongodb-mms-automation/bin':
-    ensure => 'directory',
-    owner => 'mongodb',
-    group  => 'mongodb',
-    mode   => '0775',
+    ensure  => 'directory',
+    owner   => 'mongodb',
+    group   => 'mongodb',
+    mode    => '0775',
     require => Package['mongodb-mms-automation-agent-manager'];
   '/opt/mongodb-mms-automation/versions':
-    ensure => 'directory',
-    owner => 'mongodb',
-    group  => 'mongodb',
-    mode   => '0775',
+    ensure  => 'directory',
+    owner   => 'mongodb',
+    group   => 'mongodb',
+    mode    => '0775',
     require => Package['mongodb-mms-automation-agent-manager'];
   '/var/log/mongodb-mms-automation':
-    ensure => 'directory',
-    owner => 'mongodb',
-    group  => 'mongodb',
-    mode   => '0775',
+    ensure  => 'directory',
+    owner   => 'mongodb',
+    group   => 'mongodb',
+    mode    => '0775',
     require => Package['mongodb-mms-automation-agent-manager'];
   '/var/lib/mongodb-mms-automation':
-    ensure => 'directory',
-    owner => 'mongodb',
-    group  => 'mongodb',
-    mode   => '0775',
+    ensure  => 'directory',
+    owner   => 'mongodb',
+    group   => 'mongodb',
+    mode    => '0775',
     require => Package['mongodb-mms-automation-agent-manager'];
   }
 
