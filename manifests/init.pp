5b163f2ea8df349e8cf35aaf0deb3b50cf2f2a8b
Justin Hoppensteadt <hop@nationbuilder.com>
2015-05-19 15:38:17 -0700

    restructure

--- manifests/init.pp
+++ manifests/init.pp
@@ -40,16 +40,14 @@ class mms {
   include mms::config
   include mms::service
 
-  Class['mms::install'] -> Class['mms::config'] ~> Class['mms::service']
-  Class['mms::install'] -> mms::Command <| |> ~> Class['mms::service']
+  # Anchor this as per #8040 - this ensures that classes won't float off and↩
+  # mess everything up.  You can read about this at:↩
+  # http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues↩
+  anchor { 'mms::begin': }
+  anchor { 'mms::end': }
 
-  anchor {
-    'mms::begin':
-      before => [Class['mms::install'],Class['mms::config']],
-      notify => Class['mms::service'];
-    'mms::end':
-      require => Class['mms::service'];
-  }
+  Anchor['mms::begin'] -> Class['::mms::install'] -> Class['::mms::config']
+    ~> Class['::mms::service'] -> Anchor['mms::end']
 
 }
 # vim: ft=puppet
--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,7 @@
 class mms::install inherits mms {
 
   package {'mongodb-mms-automation-agent-manager':
-    ensure => installed,
+    ensure => latest,
   }
 
 }
