9eeb8620d8f5adefea4d65e14659d4f9a66b0381
Justin Hoppensteadt <git@justinhoppensteadt.com>
2015-05-19 23:28:52 +0000

    stripped out utf8 chars

--- manifests/init.pp
+++ manifests/init.pp
@@ -40,9 +40,9 @@ class mms {
   include mms::config
   include mms::service
 
-  # Anchor this as per #8040 - this ensures that classes won't float off and↩
-  # mess everything up.  You can read about this at:↩
-  # http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues↩
+  # Anchor this as per #8040 - this ensures that classes won't float off and
+  # mess everything up.  You can read about this at:
+  # http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues
   anchor { 'mms::begin': }
   anchor { 'mms::end': }
 
